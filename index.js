2268f4c72c24d4ce57503f54f1b8c0647aaf178f
KoryNunn <knunn187@gmail.com>
2014-11-04 16:46:11 +1000

    Fixed gaffa reference

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ Carousel.prototype.render = function(){
         view.value.set(dial.value());
     });
     dial.renderItem = function(){
-        var imagePath = gaffa.gedi.utils.get(view.imagePath, this._data);
+        var imagePath = view.gaffa.gedi.utils.get(view.imagePath, this._data);
         this.element = crel('img', {src:imagePath});
         this.element.style.display = 'block';
         this.element.style.position = 'absolute';
